94fde004b292006f7dc3e4d187d9d61a02c5e067
blcook223 <benjamin.l.cook@gmail.com>
2016-05-31 10:35:26 -0400

    updated stuff

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 (function () {
   'use strict';
 
-  angular.module('skytutorial', ['sky']);
+  angular.module('skytutorial', ['sky'])
+    .run(run);
+
+
+  function run(bbPalette) {
+    var mono = bbPalette.getColorSequence(14, 'mono');
+    var multi = bbPalette.getColorSequence(14, 'multi');
+
+    console.log(mono, multi);
+
+    // console.log(bbPalette.getColorByIndex(4, 'mono')); // == #c1e1f6
+  }
+
+  run.$inject = ['bbPalette'];
 }());
